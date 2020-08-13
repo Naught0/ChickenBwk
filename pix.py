3749963fee10f52f3df3e88df32a3f9d962224a4
James <4589250+Naught0@users.noreply.github.com>
2020-08-13 11:07:52 -0400

    add status cmd

--- pix.py
+++ pix.py
@@ -1,13 +1,15 @@
 import discord
 import io
 import json
+import humanize
+import datetime
 
-from datetime import datetime
 from discord.ext import commands, tasks
 
 
 class Pix(commands.Cog):
     IMG_URL = "http://chickens.marc.cx/snapshot"
+    CHICKS_BORN = datetime.date(2020, 7, 22)
 
     def __init__(self, bot, *args, **kwargs):
         self.bot = bot
@@ -31,32 +33,56 @@ class Pix(commands.Cog):
 
         self.bot.num_pix += 1
         with open("stuff.json", "w") as f:
-            json.dump({"token": self.bot.token, "num_pix": self.bot.num_pix}, f)
+            j = json.load(f)
+            j["num_pix"] = self.bot.num_pix
+            json.dump(j, f)
 
     def cog_unload(self):
         self.background_check.cancel()
 
     @commands.command(name="stop", aliases=["disable"])
     async def _stop(self, ctx: commands.Context):
-        await ctx.message.add_reaction("👍")
+        if self.background_check.is_running():
+            em = discord.Embed(
+                title=":octagonal_sign: Chik'n pix stopped",
+                description=f"By: {ctx.author.mention}",
+                color=discord.Color.blurple(),
+            )
+            await self.channel.send(embed=em)
 
-        em = discord.Embed(
-            title=":octagonal_sign: Chik'n pix stopped",
-            description=f"By: {ctx.author.mention}",
-            color=discord.Color.blurple()
-        )
-        await self.channel.send(embed=em)
+            self.background_check.cancel()
+            await ctx.message.add_reaction("🐣")
 
-        self.background_check.cancel()
+        else:
+            await ctx.message.add_reaction("🟥")  # Red square
 
     @commands.command(name="start", aliases=["enable"])
     async def _start(self, ctx: commands.Context):
-        await ctx.message.add_reaction("👍")
-        self.background_check.start()
+        if self.background_check.is_running():
+            await ctx.message.add_reaction("🟥")  # Red square
+        else:
+            self.background_check.start()
+            await ctx.message.add_reaction("🐣")
+
+    @command.command(name="status", aliases=["stats", "info"])
+    async def _status(self, ctx: commands.Context):
+        em = discord.Embed(title=":bar_chart::hatching_chick: Chik'n Stats", color=discord.Color.gold())
+        em.add_field(
+            name=":egg::hatched_chick::chicken: Chick Age",
+            value=f"```{humanize.naturaldelta(datetime.date.today() - self.CHICKS_BORN)}```"
+        )
+        em.add_field(
+            name=":frame_photo: Total Pics",
+            value=f"```{self.bot.num_pix}```"
+        )
+        em.add_field(
+            name=":clock1: Bot Uptime",
+            value=f"```{humanize.naturaldelta(datetime.datetime.now() - self.bot.start_time)}```"
+        )
 
     @tasks.loop(seconds=10.0)
     async def background_check(self):
-        current_hour = datetime.now().hour
+        current_hour = datetime.datetime.now().hour
 
         if current_hour != self.bot.last_hour:
             self.bot.last_hour = current_hour
