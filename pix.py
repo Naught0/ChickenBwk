8b951ace2a46bb2939f892e6c82d93fe4208fec1
James <4589250+Naught0@users.noreply.github.com>
2020-09-03 11:22:00 -0400

    add time to picture filenames & react on cooldown

--- pix.py
+++ pix.py
@@ -30,7 +30,10 @@ class Pix(commands.Cog):
 
         await channel.send(
             content=":baby_chick: **CHICKEN STATUS** :baby_chick:",
-            file=discord.File(buf, "chick_pix.png"),
+            file=discord.File(
+                buf,
+                f"chick_pix{datetime.datetime.now(tz=datetime.timezone.utc).strftime('%Y%m%d%H%M%S%f')}.png",
+            ),
         )
 
         self.bot.num_pix += 1
@@ -68,34 +71,42 @@ class Pix(commands.Cog):
     async def _status(self, ctx: commands.Context):
         async with self.session.get(self.STATUS_URL) as resp:
             status_text = await resp.text()
-            status_data = json.loads(status_text.split('uptimeValues = ')[1].split(';')[0])
-        
+            status_data = json.loads(
+                status_text.split("uptimeValues = ")[1].split(";")[0]
+            )
+
         _delta = datetime.date.today() - self.CHICKS_BORN
         _weeks, _days = divmod(_delta.days, 7)
 
-        em = discord.Embed(title=":bar_chart::hatching_chick: Chik'n Stats", color=discord.Color.gold())
+        em = discord.Embed(
+            title=":bar_chart::hatching_chick: Chik'n Stats", color=discord.Color.gold()
+        )
         em.add_field(
             name=":egg::hatched_chick::chicken: Chick Age",
-            value=f"```{_weeks} weeks {_days} day(s)```"
+            value=f"```{_weeks} weeks {_days} day(s)```",
         )
-        em.add_field(
-            name=":frame_photo: Total Pics",
-            value=f"```{self.bot.num_pix}```"
-        )
+        em.add_field(name=":frame_photo: Total Pics", value=f"```{self.bot.num_pix}```")
         em.add_field(
             name=":clock1: Bot Uptime",
-            value=f"```{humanize.precisedelta(datetime.datetime.now() - self.bot.start_time)}```"
+            value=f"```{humanize.precisedelta(datetime.datetime.now() - self.bot.start_time)}```",
         )
         em.add_field(
             name=":camera: Feed Uptime % (30|60|90 day)",
-            value=f"```{status_data[0]['thirty']:.1f}% | {status_data[0]['sixty']:.1f}% | {status_data[0]['ninety']:.1f}%```"
+            value=f"```{status_data[0]['thirty']:.1f}% | {status_data[0]['sixty']:.1f}% | {status_data[0]['ninety']:.1f}%```",
         )
         em.add_field(
             name=":globe_with_meridians: Site Uptime % (30|60|90 day)",
-            value=f"```{status_data[1]['thirty']:.1f}% | {status_data[1]['sixty']:.1f}% | {status_data[1]['ninety']:.1f}%```"
+            value=f"```{status_data[1]['thirty']:.1f}% | {status_data[1]['sixty']:.1f}% | {status_data[1]['ninety']:.1f}%```",
         )
 
         await self.channel.send(embed=em)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            return await ctx.message.add_reaction("🟥")
+
+        print(f"Ignoring exception in command {ctx.command}: {error}")
 
     @tasks.loop(seconds=10.0)
     async def background_check(self):
